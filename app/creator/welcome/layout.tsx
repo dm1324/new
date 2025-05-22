5454b399969e2a4f052e9fea4f7c8566533598dd
dm1324 <167073283+dm1324@users.noreply.github.com>
2025-05-22 10:56:56 +0530

    Fix globals.css import path

--- app/creator/welcome/layout.tsx
+++ app/creator/welcome/layout.tsx
@@ -3,7 +3,7 @@ export const runtime = "edge";
 
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
+import "@/app/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
